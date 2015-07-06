cfc3cbedf9324937a5e63b7a7f88b8f783711d36
Masuzu <anveena.menethil@gmail.com>
2015-07-06 13:46:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,53 +17,8 @@ template <typename T, size_t N>
 type name##_array[] = {__VA_ARGS__}; \
                       std::set<type> name(begin(name##_array), end(name##_array));
 
-#define __TEST
-
 int main(int argc, char *argv[])
 {
-  locale::global(locale(""));
-
-#ifdef __TEST
-  int num_groups = 9;
-  regex_t *regex_compiled = new regex_t;
-  char *regex_pattern = "PAOMT : ([0-9]*)[^-]*- ([0-9]*)[^-]*- ([0-9]*)[^-]*- ([0-9]*)[^-]*- ([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?) - ([0-9]*)";
-  //char *regex_pattern = "(id=)([^\\s]*)( id2=)([^\\s]*)( value=)([^\\s]*)";
-  //char *regex_pattern = "TS VMP [^:]*:  ([0-9]*)[^[0-9]]*([0-9]*\\.[0-9]*\\.[0-9]*)\\s([0-9]*)[^[0-9]]*([0-9]*)";
-  //char *regex_pattern = "PAGC : ([0-9]*)[^-]*- ([0-9]*)[^-]*- ([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?)[^-]*- ([0-9]*)";
-  //char *regex_pattern = "resultat [^\\s]* ([^:]*):[^P]*P ([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?)MW[^Q]*Q  =   ([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?)MVAR[^S]*S  =   ([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?)MVA[^I]*I  =   ([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?)kA";
-
-  if (regcomp(regex_compiled, regex_pattern, REG_EXTENDED))
-    printf("Could not compile regular expression %s.\n", regex_pattern);
-
-  regmatch_t groups[num_groups];
-  //const char *cursor = "	TS VMP créée :  0	457.0.58186791	67	715";
-  //const char *cursor = "id=XXX id2=YYY value=ZZZ";
-  //const char *cursor = "PAOMT : 0 - 1133 - 0 - 1 - -19804.2 - 0";
-  const char *cursor = "PAOMT : 0 - 1212 - 0 - 5 - -0 - 0";
-  //const char *cursor = "PAGC : 64 	- 4 	- 42.0913 	- 0";
-  //const char *cursor = "PAGC : 64 	- 8 	- 0 	- 0";
-  //const char *cursor = "PAGC : 67 	- 211 	- -16.1271 	- 0";
-  //const char *cursor = "PAGC : 67 	- 67 	- -235.343 	- 0";
-  //const char *cursor = "PAGC : 63       - 99    - 3.2e-07       - 0";
-  //const char *cursor = "		-->  resultat départ TUILERIE:  P 1.38607MW		Q  =   0.217258MVAR		S  =   1.40299MVA 		I  =   0.0405009kA";
-  if (regexec(regex_compiled, cursor, num_groups, groups, 0))
-  {
-    // No more matches
-    std::cout << "No matches found\n";
-  }
-
-  __StdSet(int, capturing_group_idx, 2, 4, 6, 8);
-  for (int i = 0; i < num_groups; ++i)
-  {
-    char cursor_copy[strlen(cursor) + 1];
-    strcpy(cursor_copy, cursor);
-    cursor_copy[groups[i].rm_eo] = 0;
-    char *group_value = (cursor_copy + groups[i].rm_so);
-
-    printf("Capturing group[%i]: %s\n", i, group_value);
-  }
-#endif
-
   if(argc != 3)
   {
     cout << "Syntax: ./LogChecker <filename_to_check_1> <filename_to_check_2>\n";
